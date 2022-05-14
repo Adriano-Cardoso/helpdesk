00a1769f1814f8394f57db36434cb693e86fe3e5
Adriano Goulart <adrianogc@ciandt.com>
2022-05-14 20:57:10 -0300

    Primeiro commit

---
+++ helpedesk/src/test/java/com/adriano/helpedesk/HelpedeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriano.helpedesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpedeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
