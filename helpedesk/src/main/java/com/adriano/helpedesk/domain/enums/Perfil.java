e18687cb86478b13996ac6ef729df471061c2630
Adriano Goulart <adrianogc@ciandt.com>
2022-05-14 21:35:54 -0300

    Criação do PerfilEnum

---
+++ helpedesk/src/main/java/com/adriano/helpedesk/domain/enums/Perfil.java
@@ -0,0 +1,30 @@
+package com.adriano.helpedesk.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public enum Perfil {
+
+    ADMIN(0, "ROLE_ADMIN"),
+    CLIENTE(1, "ROLE_CLIENTE"),
+    TECNICO(2, "ROLE_TECNICO");
+
+    @Getter
+    private Integer codigo;
+    @Getter
+    private String descricao;
+
+    public static Perfil toEnum(Integer codigo){
+        if (codigo == null){
+            return null;
+        }
+        for (Perfil x : Perfil.values()){
+            if (codigo.equals(x.getCodigo())){
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Perfil Inválido");
+    }
+}
