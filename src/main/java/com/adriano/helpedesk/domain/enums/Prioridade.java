5a45e2d5a2be512a03691e47640d51cf812bcb01
Adriano Goulart <adrianogc@ciandt.com>
2022-05-15 16:03:39 -0300

    finalização de criação de enums

---
+++ src/main/java/com/adriano/helpedesk/domain/enums/Prioridade.java
@@ -0,0 +1,30 @@
+package com.adriano.helpedesk.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public enum Prioridade {
+
+    BAIXA(0, "BAIXA"),
+    MEDIA(1, "MEDIA"),
+    ALTA(2, "ALTA");
+
+    @Getter
+    private Integer codigo;
+    @Getter
+    private String descricao;
+
+    public static Prioridade toEnum(Integer codigo){
+        if (codigo == null){
+            return null;
+        }
+        for (Prioridade x : Prioridade.values()){
+            if (codigo.equals(x.getCodigo())){
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Prioridade Inválida");
+    }
+}
---
+++ src/main/java/com/adriano/helpedesk/domain/enums/Status.java
@@ -0,0 +1,30 @@
+package com.adriano.helpedesk.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public enum Status {
+
+    ABERTO(0, "ABERTO"),
+    ANDAMENTO(1, "ANDAMENTO"),
+    ENCERRADO(2, "ENCERRADO");
+
+    @Getter
+    private Integer codigo;
+    @Getter
+    private String descricao;
+
+    public static Status toEnum(Integer codigo){
+        if (codigo == null){
+            return null;
+        }
+        for (Status x : Status.values()){
+            if (codigo.equals(x.getCodigo())){
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Status Inválido");
+    }
+}
