dacd42c6a49900bf663191eade0f40c7918979eb
Adriano Goulart <adrianogc@ciandt.com>
2022-05-14 21:22:34 -0300

    primeiro commit

---
+++ helpedesk/src/main/java/com/adriano/helpedesk/HelpedeskApplication.java
@@ -0,0 +1,13 @@
+package com.adriano.helpedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpedeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpedeskApplication.class, args);
+	}
+
+}
